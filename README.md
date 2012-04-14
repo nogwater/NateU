8038baf62d9d478fa756fb1b2cc88a0c05ed87d7
Aaron McBride <nogwater@gmail.com>
2012-04-14 16:26:44 -0700

    basic MUS to NOTE compiler implemented for node.js

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Code for Nathan's University PL101 class.
+
+http://nathansuniversity.com/
+
+* mus/ - a MUS to NOTE compiler
---
+++ mus/compiler.js
@@ -0,0 +1,58 @@
+
+// MUS compiler for PL101
+
+// calculates the end time for a given expression and start time
+var endTime = function (time, expr) {
+    if (expr.tag === 'note') {
+        return time + expr.dur;
+    } else if (expr.tag === 'seq') {
+        return time + endTime(0, expr.left) + endTime(0, expr.right);
+    } else if (expr.tag === 'par') {
+        var durLeft = endTime(0, expr.left);
+        var durRight = endTime(0, expr.right);
+        return time + Math.max(durLeft, durRight) ;
+    }
+};
+
+// compiles the given musexpress (and optional startTime) into a NOTE program
+var compile = function (musexpr, startTime) {
+    if (!startTime) { startTime = 0; }
+    var noteExpr = [];
+    var noteRight, i;
+    if (musexpr.tag === 'note') {
+        noteExpr.push({ tag: 'note', pitch: musexpr.pitch, start: startTime, dur: musexpr.dur });
+    } else if (musexpr.tag === 'seq') {
+        noteExpr = compile(musexpr.left, startTime);
+        var durLeft = endTime(startTime, musexpr.left);
+        noteRight = compile(musexpr.right, durLeft);
+        for (i = 0; i < noteRight.length; i++) {
+            noteExpr.push(noteRight[i]);
+        }
+    } else if (musexpr.tag === 'par') {
+        noteExpr = compile(musexpr.left, startTime);
+        noteRight = compile(musexpr.right, startTime);
+        for (i = 0; i < noteRight.length; i++) {
+            noteExpr.push(noteRight[i]);
+        }
+    }
+    return noteExpr;
+};
+
+
+// some tests
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log('melody_mus:');
+console.log(melody_mus);
+
+console.log('compiled:');
+console.log(compile(melody_mus));
