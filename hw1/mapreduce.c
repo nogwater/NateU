28ad804e82814bb11685e9d8d0f86fe73774a9d9
Aaron McBride <nogwater@gmail.com>
2012-04-23 08:03:24 -0700

    incorporated Suncho's comments for idiomatic C

--- hw1/mapreduce.c
+++ hw1/mapreduce.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#define SIZE 5
+
+/*
+  improvements thanks to Suncho
+  http://nathansuniversity.com/vanilla/discussion/comment/1019#Comment_1019
+*/
+
 int twice(int n)
 {
     return 2 * n;
@@ -10,39 +17,39 @@ int add(int a, int b)
 	return a + b;
 }
 
-void map(int *input, int *output, int size, int(*fp)(int))
+void map(const int *input, int *output, int size, int(*fp)(int))
 {
-	if (size > 0) {
-		*output = fp(input[0]);
-		map(input+1, output+1, size-1, fp);
+	while (size-- > 0) {
+		*output++ = fp(*input++);
 	}
 }
 
 int reduce(int acc, int *data, int size, int(*fp)(int, int))
 {
-	if (size < 1) {
-		return acc;
-	} else {
-		return reduce(fp(acc, *data), data+1, size-1, fp);
+	while (size-- > 0) {
+		acc = fp(acc, *data++);
 	}
+	return acc;
 }
-
 
 int main(int argc, char *argv[])
 {
-	int size = 5;
-	int input[] = {1, 2, 3, 4, 5};
-	int output[size];
-
-	map(input, output, size, twice);
-
-	printf("Mapped:\n");
+	int input[SIZE] = {1, 2, 3, 4, 5};
+	int output[SIZE];
 	int i = 0;
-	for (i = 0; i < size; i++) {
+	int r = 0;
+
+	map(input, output, SIZE, twice);
+
+	printf("%s", "Mapped:\n");
+	i = 0;
+	for (i = 0; i < SIZE; i++) {
 		printf("%d->%d ", input[i], output[i]);
 	}
 	printf("\n");
 
-	int r = reduce(0, output, size, add);
+	r = reduce(0, output, SIZE, add);
 	printf("Reduced: %d\n", r);
+
+	return 0;
 }
