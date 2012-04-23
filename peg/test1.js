f557b7c53de269651d669eebbf6431b8e95ecdb6
Aaron McBride <nogwater@gmail.com>
2012-04-22 23:51:25 -0700

    added more test cases

--- peg/test1.js
+++ peg/test1.js
@@ -10,23 +10,30 @@ console.log(data);
 var parse = PEG.buildParser(data).parse;
 // Do a test
 
-// Simple list
-assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+// Simple stuff
+assert.deepEqual( parse("a"), "a", "Single atom");
+assert.deepEqual( parse("()"), [], "Empty list");
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"], "Simple list");
+assert.deepEqual( parse("(a () (1 2))"), ["a", [], [1, 2]], "Mixed list");
 
 // Extra trailing whitespace
-assert.deepEqual( parse("(a  b  	c ) "), ["a", "b", "c"] );
-assert.deepEqual( parse("(a  b  (c ) ) "), ["a", "b", ["c"]] );
+assert.deepEqual( parse("(a  b  	c )"), ["a", "b", "c"], "Trailing whitespace (atom)");
+assert.deepEqual( parse("(a b c () )"), ["a", "b", "c", []], "Trailing whitespace (empty list)");
+assert.deepEqual( parse("(a b c ) "), ["a", "b", "c"], "Trailing whitespace (non-empty list)");
+assert.deepEqual( parse("(a b '(1 2)  )"), ['a', 'b', ['quote', ['1', '2']]], "Trailing whitespace (quote)2");
 
 // Extra leading whitespace
-assert.deepEqual( parse(" (a  b  (  c ) ) "), ["a", "b", ["c"]] );
+assert.deepEqual( parse(" ( a  b  (  c ) ) "), ["a", "b", ["c"]], "Leading whitespace");
 
 // Quote
-assert.deepEqual( parse("'(1 2 3)"), parse("(quote (1 2 3))") );
+assert.deepEqual( parse("'a"), parse("(quote a)"), "Atom quoting");
+assert.deepEqual( parse("'(1 2 3)"), parse("(quote (1 2 3))"), "List quoting");
 
 // Newline as whitespace
-assert.deepEqual( parse("(a\nb\nc)"), ["a", "b", "c"] );
+assert.deepEqual( parse("(a\nb\nc)"), ["a", "b", "c"], "Newline whitespace");
 
 // Comment lines
+// Note: ";;" must be the first character in the line to be a newline
 assert.deepEqual( parse("(keep;; ;; \n;; comment line\natom\n)"), ["keep;;", ";;", "atom"])
 
 console.log("-----------------------");
