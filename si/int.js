dfffb49632e1b2f2b0dc089feef377e497c72ec2
Aaron McBride <nogwater@gmail.com>
2012-05-02 17:42:58 -0700

    added define and set! commands

--- si/int.js
+++ si/int.js
@@ -11,6 +11,7 @@ var parse = PEG.buildParser(data).parse;
 // Do tests
 
 var evalScheem = function (expr, env) {
+    var name, value;
     // Numbers evaluate to themselves
     if (typeof expr === 'number') {
         return expr;
@@ -29,9 +30,31 @@ var evalScheem = function (expr, env) {
             return evalScheem(expr[1], env) * evalScheem(expr[2], env);
         case '/':
             return evalScheem(expr[1], env) / evalScheem(expr[2], env);
+        case 'define':
+            name = expr[1]; // might be cool to allow references (expressions that return strings?)
+            value = evalScheem(expr[2], env);
+            // should probably error if env[name] is NOT undefined
+            env[name] = value;
+            return 0; // why not return the value?
+        case 'set!':
+            name = expr[1];
+            value = evalScheem(expr[2], env);
+            // should probably error if env[name] is undefined
+            env[name] = value;
+            return 0; // why not return the value?
     }
 };
 
 assert.deepEqual( evalScheem(['+', 5, ['*', 2, 3]], {}), 11, 'Basic math');
 
 assert.deepEqual( evalScheem(['+', 'x', 1], {'x': 5}), 6, 'Add with variable');
+
+var env = {a: 2, b: 7};
+var result = evalScheem(['define', 'x', 5], env);
+assert.deepEqual(env , {a: 2, b: 7, x: 5}, 'define a variable');
+assert.deepEqual(result, 0, 'define result is 0')
+
+var env = {a: 2, b: 7};
+var result = evalScheem(['set!', 'a', 3], env);
+assert.deepEqual(env , {a: 3, b: 7}, 'set! a variable');
+assert.deepEqual(result, 0, 'set! result is 0')
